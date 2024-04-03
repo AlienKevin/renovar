765c9738f278578b89d75c8ad7570ad96f60f1e7
Kevin Li <kevinli020508@gmail.com>
2024-04-03 09:45:09 -0400

    Can change floor material by clicking A and B buttons on right controller

--- Assets/material_controller.cs
+++ Assets/material_controller.cs
@@ -1,13 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class material_controller : MonoBehaviour
+public class MaterialController : MonoBehaviour
 {
-    public void select_floor_material(Material material)
+    //private OVRSceneManager ovrSceneManager;
+
+    //private void Awake()
+    //{
+    //    ovrSceneManager = FindObjectOfType<OVRSceneManager>();
+    //}
+
+    private void set_material(string label, Material material)
     {
-        Debug.Log("select_floor_material: " + material);
-        var floor_mesh_render = GameObject.Find("FloorOverride").GetComponent<MeshRenderer>();
-        floor_mesh_render.materials[0] = material;
+        List<OVRSceneAnchor> anchors = new();
+        OVRSceneAnchor.GetSceneAnchors(anchors);
+        foreach (var anchor in anchors)
+        {
+            Debug.Log("anchor.name: " + anchor.name);
+            if (anchor.name.StartsWith(label + "Override"))
+            {
+                anchor.gameObject.GetComponent<MeshRenderer>().material = material;
+            }
+        }
+
+        //foreach (var @override in ovrSceneManager.PrefabOverrides)
+        //{
+        //    if (@override.ClassificationLabel == label)
+        //    {
+        //        Debug.Log("@override.Prefab.gameObject:", @override.Prefab.gameObject);
+        //        @override.Prefab.gameObject.GetComponent<MeshRenderer>().material = material;
+        //        break;
+        //    }
+        //}
+    }
+
+    public void set_floor_material(Material material)
+    {
+        set_material("Floor", material);
     }
 }
